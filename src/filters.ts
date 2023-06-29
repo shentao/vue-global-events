f305def148758da35ad32444fbfdfcdc76f4f2d1
Eduardo San Martin Morote <posva13@gmail.com>
2023-06-29 13:25:01 +0200

    feat: add include and exclude filters

--- src/filters.ts
+++ src/filters.ts
@@ -1,5 +1,30 @@
 // common filters for global events
 
-export function excludeElements() {
-  // TODO: implement
+/**
+ * Creates a filter than can be passed to `<GlobalEvents />` to exclude events from elements with the given tag names.
+ *
+ * @param tagNames - array of tag names to exclude
+ */
+export function excludeElements(tagNames: Array<Uppercase<_HTMLElementNames>>) {
+  return (event: Event) => {
+    const target = event.target as HTMLElement
+    return !(tagNames as string[]).includes(target.tagName)
+  }
 }
+
+/**
+ * Creates a filter than can be passed to `<GlobalEvents />` to include events from elements with the given tag names.
+ *
+ * @see excludeElements
+ *
+ * @param tagNames - array of tag names to include
+ */
+export function includeElements(tagNames: Array<Uppercase<_HTMLElementNames>>) {
+  return (event: Event) => {
+    const target = event.target as HTMLElement
+    return (tagNames as string[]).includes(target.tagName)
+  }
+}
+
+// @internal
+export type _HTMLElementNames = keyof HTMLElementTagNameMap
